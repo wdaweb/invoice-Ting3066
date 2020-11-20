4e96906997eaea5b8fd3037c8623e9a306c4a996
WenTing <mybaby0103066@gmail.com>
2020-11-20 11:01:58 +0800

    完成find()函示，可利用id或陣列作為查詢條件

--- test.php
+++ test.php
@@ -2,16 +2,33 @@
 include_once "base.php";
 
 
+$array=['欄位1'=>'值1','欄位2'=>'值2','id'=>'9'];
+
+//利用暫時的陣列存放語句
+foreach($array as $key => $value){
+  $tmp[]=sprintf("`%s`='%s'",$key,$value);
+  // $tmp[]="`".$key."`='".$value."'";
+
+}
+//使用implode將陣列中的語句串成語法
+echo implode(" && ",$tmp);
+echo "<br>";
+
+
 
 function find($table,$id){
   global $pdo;
+  //判斷id是否為數字
+  if(is_array($id)){
 
-  if(is_numeric($id)){
-    $sql="select * from $table where id='$id'";
-    
-    
+    foreach($id as $key => $value){
+      $tmp[]=sprintf("`%s`='%s'",$key,$value);
+      // $tmp[]="`".$key."`='".$value."'";
+    }
+
+    $sql="select * from $table where ".implode(" && ",$tmp);
   }else{
-    $sql="select * from $table where $id";
+    $sql="select * from $table where id='$id'";
     
   }
 
@@ -19,17 +36,12 @@ function find($table,$id){
   return $row;
 
 }
-// function find2($table,$def){
-//   global $pdo;
-//   $sql="select * from $table where $def";
-//   $row=$pdo->query($sql)->fetch();
-//   return $row;
-// }
+
 
 $row=find('invoices',8);
 echo $row['code'].$row['number']."<br>";
 
-$row=find('invoices',"code='KJ' && number='33236629'");
+$row=find('invoices',['code'=>'KJ','number'=>'33236629']);
 echo $row['code'].$row['number']."<br>";
 
 $row=find('invoices',19);
