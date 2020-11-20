ff22839333d6c300746e80eaa5f8d81b625ea9c1
WenTing <mybaby0103066@gmail.com>
2020-11-20 15:52:36 +0800

    增加all()函式來取得多筆資料

--- test.php
+++ test.php
@@ -2,49 +2,105 @@
 include_once "base.php";
 
 
-$array=['欄位1'=>'值1','欄位2'=>'值2','id'=>'9'];
+//取得單一資料的自訂函式
+// function find($table,$id){
+//   global $pdo;
+//   //判斷id是否為數字
+//   if(is_array($id)){
 
-//利用暫時的陣列存放語句
-foreach($array as $key => $value){
-  $tmp[]=sprintf("`%s`='%s'",$key,$value);
-  // $tmp[]="`".$key."`='".$value."'";
+//     foreach($id as $key => $value){
+//       $tmp[]=sprintf("`%s`='%s'",$key,$value);
+//       // $tmp[]="`".$key."`='".$value."'";
+//     }
 
-}
-//使用implode將陣列中的語句串成語法
-echo implode(" && ",$tmp);
-echo "<br>";
-
-
-
-function find($table,$id){
-  global $pdo;
-  //判斷id是否為數字
-  if(is_array($id)){
-
-    foreach($id as $key => $value){
-      $tmp[]=sprintf("`%s`='%s'",$key,$value);
-      // $tmp[]="`".$key."`='".$value."'";
-    }
-
-    $sql="select * from $table where ".implode(" && ",$tmp);
-  }else{
-    $sql="select * from $table where id='$id'";
+//     $sql="select * from $table where ".implode(" && ",$tmp);
+//   }else{
+//     $sql="select * from $table where id='$id'";
     
+//   }
+
+//   $row=$pdo->query($sql)->fetch();
+//   return $row;
+
+// }
+
+//arg 為一陣列型態，即使為空值型態依舊是陣列，且程式照樣執行
+function all($table,...$arg){
+  global $pdo;
+
+  $sql="select * from $table ";
+  if(isset($arg[0])){
+    if(is_array($arg[0])){
+      //製作會在where 號面的句子字串
+      if(!empty($arg[0])){
+        foreach($arg[0] as $key => $value){
+          $tmp[]=sprintf("`%s`='%s'",$key,$value);
+          // $tmp[]="`".$key."`='".$value."'";
+        }
+
+        $sql=$sql." where ".implode(" && ",$tmp);
+      }
+  
+    }else{
+  
+        $sql=$sql.$arg[0];
+  
+  
+    }
   }
+  
+  if(isset($arg[1])){
+    //製作接在最後面的句子字串
+    $sql=$sql.$arg[1];
 
-  $row=$pdo->query($sql)->fetch();
-  return $row;
 
+  }
+  echo $sql."<br>";
+
+  return $pdo->query($sql)->fetchALL();
+  function del($table,$id){
+    $sql="delete * from $table where ";
+    if(is_array($id)){
+        foreach($id as $key => $value){
+          $tmp[]=sprintf("`%s`='%s'",$key,$value);
+        }
+
+        $sql=$sql.implode(" && ",$tmp);
+  
+    }else{
+  
+        $sql=$sql." id='$id' ";
+  
+  
+    }
+    $row=$pdo->exec($sql);
+    return $row;
+  }
+  $def=['code'=>'GD'];
+  echo del('invoices',$def);
 }
 
+  
+  
 
-$row=find('invoices',8);
-echo $row['code'].$row['number']."<br>";
 
-$row=find('invoices',['code'=>'KJ','number'=>'33236629']);
-echo $row['code'].$row['number']."<br>";
 
-$row=find('invoices',19);
-echo $row['code'].$row['number'];
+  
 
+echo "<hr>";
+all('invoices');
+echo "<hr>";
+all('invoices',['code'=>"KJ",'period'=>6]);
+echo "<hr>";
+all('invoices',['code'=>"AB",'period'=>1]," order by date desc");
+echo "<hr>";
+all('invoices'," limit 5");
+// echo "<hr>";
+// print_r(all('invoices'));
+// echo "<hr>";
+// print_r(all('invoices',['code'=>"GD",'period'=>6]));
+// echo "<hr>";
+// print_r(all('invoices',['code'=>"AB",'period'=>1," order by date desc"]));
+// echo "<hr>";
+// print_r(all('invoices',"limit 5"));
 ?>
