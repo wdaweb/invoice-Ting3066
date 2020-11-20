abe837db0a7d36a4cc370ae9ad9818ab849a0779
WenTing <mybaby0103066@gmail.com>
2020-11-20 10:01:38 +0800

    修改find()的參數資料形式,可以判斷資料型態來決定不同的SQL語句內容

--- test.php
+++ test.php
@@ -1,13 +1,38 @@
 <?php
 include_once "base.php";
 
-find('invoices',"id='9'");
 
-function find($table,$def){
+
+function find($table,$id){
   global $pdo;
-  $sql="select * from $table where $def";
+
+  if(is_numeric($id)){
+    $sql="select * from $table where id='$id'";
+    
+    
+  }else{
+    $sql="select * from $table where $id";
+    
+  }
+
   $row=$pdo->query($sql)->fetch();
   return $row;
+
 }
+// function find2($table,$def){
+//   global $pdo;
+//   $sql="select * from $table where $def";
+//   $row=$pdo->query($sql)->fetch();
+//   return $row;
+// }
+
+$row=find('invoices',8);
+echo $row['code'].$row['number']."<br>";
+
+$row=find('invoices',"code='KJ' && number='33236629'");
+echo $row['code'].$row['number']."<br>";
+
+$row=find('invoices',19);
+echo $row['code'].$row['number'];
 
 ?>
